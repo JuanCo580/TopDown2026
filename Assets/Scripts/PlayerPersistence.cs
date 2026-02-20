745ad6cfcf3b2faa4a9c2b88fc308658542b9180
Juan Camilo Salgado <99854020+JuanCo580@users.noreply.github.com>
2026-02-20 17:13:53 -0500

    cambio

--- Assets/Scripts/PlayerPersistence.cs
+++ Assets/Scripts/PlayerPersistence.cs
@@ -1,9 +1,7 @@
 using UnityEngine;
-
 public class PlayerPersistence : MonoBehaviour
 {
     public static PlayerPersistence instance;
-
     private void Awake()
     {
         if (instance == null)
